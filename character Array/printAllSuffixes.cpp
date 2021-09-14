08301e0be2f357b17392fe4b1a2741152666e6b0
anand346 <rajanand9039@gmail.com>
2021-09-14 21:18:25 +0530

    Date : 14-09-2021 , Time : 21:18

---
+++ character Array/printAllSuffixes.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+int main(){
+    char str[100];
+    cout<<"Enter your name : ";
+    cin>>str;
+    for(int i = 0; i < strlen(str); i++){
+        for(int j = (strlen(str) - i )- 1; j < strlen(str); j++){
+            if(j < 0){
+                return 0;
+            }else{
+                cout<<str[j]<<" ";
+            }       
+        }
+        cout<<endl;
+    }
+}
