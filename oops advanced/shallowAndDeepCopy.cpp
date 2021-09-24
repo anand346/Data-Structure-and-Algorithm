61a8b151a8187abb409fdf64a18e4ef67f4935a0
anand346 <rajanand9039@gmail.com>
2021-09-24 11:30:24 +0530

    24-09-2021 11:30

--- oops advanced/shallowAndDeepCopy.cpp
+++ oops advanced/shallowAndDeepCopy.cpp
@@ -2,10 +2,16 @@
 #include <cstring>
 using namespace std;
 class Student{
-    int age;
-    char *name;
+    
 
     public :
+        int age;
+        char *name;
+        Student(Student const &s){
+            this->age = s.age;
+            this->name = new char[strlen(s.name) + 1];
+            strcpy(this->name,s.name);
+        }
         Student(int age , char *name){
             this->age = age;
             // this->name = name; // shallow copy
@@ -24,4 +30,8 @@ int main(){
     Student s2(20,name);
     s2.print();
     s1.print();
+    Student s3(s1);
+    s3.name[0] = 'b';
+    s3.print();
+    s1.print(); //copy constructor will always make shallow copy
 }
