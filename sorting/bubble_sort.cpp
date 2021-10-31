0b8f2cdd228c74b5965bc465cc74576f838e4246
anand raj <rajanand9039@gmail.com>
2021-10-31 11:03:35 +0530

    added basic sorting algorithm

--- sorting/bubble_sort.cpp
+++ sorting/bubble_sort.cpp
@@ -2,9 +2,9 @@
 #include<bits/stdc++.h>
 using namespace std;
 void bubbleSort(int a[],int n){
-    for(int count = 1; count < n-1; count++){
+    for(int count = 1; count <= n-1; count++){
         int flag = 0;
-        for(int j = 0; j < n-2; j++){
+        for(int j = 0; j <= n-2; j++){
             if(a[j] > a[j+1]){
                 swap(a[j],a[j+1]);
                 flag = 1;
